bc16ca600f2bec5f7231a4e49cc93b3fda9e0987
astowner <the1samevl@gmail.com>
2024-08-23 20:08:26 +0300

    Add coupon

--- myshop/settings.py
+++ myshop/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'coupons.apps.CouponsConfig',
     'payment.apps.PaymentConfig',
     'orders.apps.OrdersConfig',
     'cart.apps.CartConfig',
