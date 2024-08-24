c78208f1c5a4d19c529c4561fe79f41e38be0ed7
astowner <the1samevl@gmail.com>
2024-08-24 17:14:32 +0300

    Add localization for Product model

--- shop/models.py
+++ shop/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.urls import reverse
+from parler.models import TranslatableModel, TranslatedFields
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=155)
-    slug = models.SlugField(max_length=155)
+class Category(TranslatableModel):
+    translations = TranslatedFields(
+        name = models.CharField(max_length=200),
+        slug = models.SlugField(max_length=200, unique=True)
+    )
 
     class Meta:
-        ordering = ["name"]
-        indexes = [
-            models.Index(fields=["name"])
-        ]
+        # ordering = ["name"]
+        # indexes = [
+        #     models.Index(fields=["name"])
+        # ]
         verbose_name = "category"
         verbose_name_plural = "categories"
     
@@ -20,15 +23,17 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse("shop:product_list_by_category", args=[self.slug,])
     
-class Product(models.Model):
+class Product(TranslatableModel):
+    translations = TranslatedFields(
+        name = models.CharField(max_length=200), 
+        slug = models.SlugField(max_length=200),
+        description = models.TextField(blank=True)
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name="products"
     )
-    name = models.CharField(max_length=155)
-    slug = models.SlugField(max_length=155)
-    description = models.TextField(blank=True)
     image = models.ImageField(upload_to="products/%Y/%m/%d/", blank=True)
     available = models.BooleanField(default=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -36,10 +41,10 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["name"]
+        # ordering = ["name"]
         indexes = [
-            models.Index(fields=["id", "slug"]),
-            models.Index(fields=["name"]),
+            # models.Index(fields=["id", "slug"]),
+            # models.Index(fields=["name"]),
             models.Index(fields=["-created"])
         ]
     
